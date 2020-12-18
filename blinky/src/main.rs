50adbb82be0a5c2ca298395c49812e0769bbc9ee
Yusef Karim <yusefkarim@riseup.net>
2020-12-18 10:12:31 -0500

    Blinky!

--- blinky/src/main.rs
+++ blinky/src/main.rs
@@ -7,8 +7,8 @@
 #![no_main]
 extern crate panic_halt;
 
-use riscv_rt::entry;
 use gd32vf103_pac;
+use riscv_rt::entry;
 
 #[entry]
 fn main() -> ! {
@@ -17,21 +17,38 @@ fn main() -> ! {
     let gpioa = board_peripherals.GPIOA;
     let gpioc = board_peripherals.GPIOC;
 
-
     rcu.apb2en.write(|w| w.paen().set_bit().pcen().set_bit());
-    unsafe { gpioc.ctl1.write(|w| w.md13().bits(0b10).ctl13().bits(0b00)); }
-    unsafe { gpioa.ctl0.write(|w| w.md1().bits(0b10).ctl1().bits(0b00)
-                                   .md2().bits(0b10).ctl2().bits(0b00)); }
-    gpioa.octl.write(|w| w.octl1().set_bit());   // OFF
-    gpioa.octl.write(|w| w.octl2().set_bit());   // OFF
-    gpioc.octl.write(|w| w.octl13().set_bit());  // OFF
+    unsafe {
+        gpioc.ctl1.write(|w| w.md13().bits(0b10).ctl13().bits(0b00));
+    }
+    unsafe {
+        gpioa.ctl0.write(|w| {
+            w.md1()
+                .bits(0b10)
+                .ctl1()
+                .bits(0b00)
+                .md2()
+                .bits(0b10)
+                .ctl2()
+                .bits(0b00)
+        });
+    }
+    gpioa.octl.write(|w| w.octl1().set_bit()); // OFF
+    gpioa.octl.write(|w| w.octl2().set_bit()); // OFF
+    gpioc.octl.write(|w| w.octl13().set_bit()); // OFF
 
     loop {
         for _ in 0..50_000 {}
-        gpioa.octl.write(|w| w.octl1().clear_bit());  // ON
+        gpioa.octl.write(|w| w.octl1().clear_bit()); // ON
         for _ in 0..50_000 {}
-        gpioa.octl.write(|w| w.octl2().clear_bit());  // ON
+        gpioa.octl.write(|w| w.octl2().clear_bit()); // ON
         for _ in 0..50_000 {}
         gpioc.octl.write(|w| w.octl13().clear_bit()); // ON
+        for _ in 0..50_000 {}
+        gpioa.octl.write(|w| w.octl1().set_bit()); // ON
+        for _ in 0..50_000 {}
+        gpioa.octl.write(|w| w.octl2().set_bit()); // ON
+        for _ in 0..50_000 {}
+        gpioc.octl.write(|w| w.octl13().set_bit()); // ON
     }
 }
